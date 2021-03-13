2b88a99aa7a76904c942ee321ccb1c8f44083356
aheidorn <allen.heidorn@gmail.com>
2021-03-13 10:12:27 -0600

    Added variable scaling to kkDrvice

--- src/main/java/frc/robot/commands/CommandKKDriveTrain.java
+++ src/main/java/frc/robot/commands/CommandKKDriveTrain.java
@@ -11,7 +11,9 @@ import frc.robot.subsystems.*;
 
 public class CommandKKDriveTrain extends CommandDriveTrain {
 
-    public double scaling = 1.0;
+    public boolean scalingOn = false;
+    public double scaling = 0.5;
+
 
     public CommandKKDriveTrain(DriveTrain driveTrain, XboxController controller) {
         super(driveTrain, controller);
@@ -27,11 +29,23 @@ public class CommandKKDriveTrain extends CommandDriveTrain {
     // Called repeatedly when this Command is scheduled to run
     @Override
     public void execute() {
+
+        int pov = m_controller.getPOV();
+        if (pov == 0) {
+            scaling += 0.1;
+            if (scaling > 1.0)
+                scaling = 1.0;
+        }
+        else if (pov == 180) {
+            scaling -= 0.1;
+            if (scaling < 0.1)
+                scaling = 0.1;
+        }
+
         boolean leftStickDown = m_controller.getStickButton(Hand.kLeft);
         if (!_wasLeftStickDown && leftStickDown) {
-            
-            double scaling = m_driveTrain.getDriveScaling() < 1.0 ? 1.0 : 0.5;
-            m_driveTrain.setDriveScaling(scaling);
+            scalingOn = !scalingOn;
+            m_driveTrain.setDriveScaling(scalingOn ? scaling : 1.0);
         }
         _wasLeftStickDown = leftStickDown;
 
@@ -44,6 +58,7 @@ public class CommandKKDriveTrain extends CommandDriveTrain {
     @Override
     public void end(boolean interrupted) {
         super.end(interrupted);
+        m_driveTrain.setDriveScaling(1.0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
